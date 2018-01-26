2bb0cfc3aed55354425171ac30b68d6a7abb3cbe
souaibou <ndiayevito04@gmail.com>
2018-01-26 01:11:16 +0000

    add different component fils

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HomeRouting} from "./home.routing";
+import { AccueilComponent } from './accueil/accueil.component';
+import { CodifierComponent } from './codifier/codifier.component';
+import { ReserverComponent } from './reserver/reserver.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRouting
   ],
-  declarations: []
+  declarations: [AccueilComponent, CodifierComponent, ReserverComponent]
 })
 export class HomeModule { }
