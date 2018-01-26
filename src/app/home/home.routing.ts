a610139891f45a85cc87ea8225fb806bcac56251
souaibou <ndiayevito04@gmail.com>
2018-01-26 01:11:39 +0000

    add picture logo

--- src/app/home/home.routing.ts
+++ src/app/home/home.routing.ts
@@ -4,6 +4,9 @@
 import {Routes, RouterModule} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {HomeComponent} from "./home.component";
+import {AccueilComponent} from "./accueil/accueil.component";
+import {CodifierComponent} from "./codifier/codifier.component";
+import {ReserverComponent} from "./reserver/reserver.component";
 /**
  * Created by souaibou on 5/9/17.
  */
@@ -12,7 +15,18 @@ const HOME_ROUTES : Routes =[
     path: 'home',
     component : HomeComponent,
     children : [
-
+      {
+        path: 'accueil',
+        component: AccueilComponent
+      },
+      {
+        path: 'codifier',
+        component: CodifierComponent
+      },
+      {
+        path: 'reserver',
+        component: ReserverComponent
+      }
     ]
   }
 
